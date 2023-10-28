939dba4f333414bf692b948c0d23beba75114ffb
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-10-28 10:59:33 +0530

    initial commit

---
+++ spring-boot/src/main/java/interview/programs/StringFormatting.java
@@ -0,0 +1,17 @@
+package interview.programs;
+
+public class StringFormatting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String 	name = "Raju";
+String country = "India";
+String technology = "Java";
+System.out.println(String.format("Hi, My name is %s.\nI am from %s and I'm using Technology %s\n",name,country,technology ));
+
+System.out.printf("Hi, My name is %s.\nI am from %s and I'm using Technology %s",name,country,technology);
+
+
+	}
+
+}
