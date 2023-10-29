5b9a76427f6f16ba0a2f1c88938c87b29894bbc3
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-10-29 12:20:39 +0530

    updated scanner

--- spring-boot/src/main/java/arrays/collections/exceptions/treads/hashmap.java
+++ spring-boot/src/main/java/arrays/collections/exceptions/treads/hashmap.java
@@ -5,29 +5,27 @@ import java.util.Map;
 
 public class hashmap {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-public static void main(String[] args) {
-	// TODO Auto-generated method stub
+		Map<Integer, String> map = new HashMap<>();
 
-	Map<Integer, String> map = new HashMap<>();
-	
-	map.put(23,"prasad");
-	map.put(24,"vijay");
-	map.put(45, "Raju");
-	map.put(78, "Venky");
-	
-	System.out.println(map.get(78));
-	System.out.println(map);
-	for(Map.Entry<Integer,String> entry  : map.entrySet()) {
-		System.out.println(entry.getKey()+" "+entry.getValue());
+		map.put(23, "prasad");
+		map.put(24, "vijay");
+		map.put(45, "Raju");
+		map.put(78, "Venky");
+
+		System.out.println(map.get(78));
+		System.out.println(map);
+		for (Map.Entry<Integer, String> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+		System.out.println("-----");
+		for (Integer key : map.keySet()) {
+			System.out.println(key + " " + map.get(key));
+		}
+
+		map.entrySet().stream().forEach(e -> System.out.print(e.getKey() + " " + e.getValue()));
+
 	}
-	System.out.println("-----");
-	for(Integer key : map.keySet()) {
-		System.out.println(key+" "+map.get(key));
-	}
-    
-	map.entrySet().stream().forEach(e -> System.out.print(e.getKey() + " " + e.getValue()));
-	
-}
-
 }
--- spring-boot/src/main/java/fibonacciTraining.java
+++ spring-boot/src/main/java/fibonacciTraining.java
@@ -5,8 +5,8 @@ public class fibonacciTraining {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int a=0, b=1,c = 0;
-System.out.println("enter a number to show fibonacci series up to ");
-int n= Utility.readNumber();
+//System.out.println("enter a number to show fibonacci series up to ");
+int n= Utility.readNumber("enter a number to show fibonacci series up to ");
 System.out.print(a+","+b);
 for(int i=1;i<n-1;i++) {
 	c=a+b;
