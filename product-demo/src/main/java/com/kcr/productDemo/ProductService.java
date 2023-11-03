77a1bfcaef49724fc062e3e49d24c0861483771e
chinnamrajukasina <chinnamrajukaina@gmail.com>
2023-11-03 19:38:19 +0530

    updated spring boot project

---
+++ product-demo/src/main/java/com/kcr/productDemo/ProductService.java
@@ -0,0 +1,10 @@
+package com.kcr.productDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductService {
+	List<Product> product = new ArrayList<>();
+	
+
+}
--- spring-boot/src/main/java/interview/programs/Flames.java
+++ spring-boot/src/main/java/interview/programs/Flames.java
@@ -8,25 +8,21 @@ public class Flames {
 		// TODO Auto-generated method stub
 		flames();
 
-		
-		
-		
 	}
 
 	private static void flames() {
 		// TODO Auto-generated method stub
-	String Str1= Utility.readString();
-	String Str2= Utility.readString();
-	System.out.println(Str1);
-	System.out.println(Str2);
-	removeDuplicates(Str1,Str2);
-	
-		
+		String Str1 = Utility.readString();
+		String Str2 = Utility.readString();
+		System.out.println(Str1);
+		System.out.println(Str2);
+		removeDuplicates(Str1, Str2);
+
 	}
 
 	private static void removeDuplicates(String str1, String str2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
