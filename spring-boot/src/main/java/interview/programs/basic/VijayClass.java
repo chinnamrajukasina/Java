a151fca609410b955b1883677a884a2f4cb5be35
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-11-08 17:01:03 +0530

    daily practice

--- spring-boot/src/main/java/interview/programs/basic/VijayClass.java
+++ spring-boot/src/main/java/interview/programs/basic/VijayClass.java
@@ -1,22 +1,21 @@
+package interview.programs.basic;
 
 public class VijayClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("Hello World");
 		String name = "rajs";
 		if (name.length() == 5) {
 			System.out.println("my name has 5 characters");
 		}
-		
-		int i =0;
-		
-		for ( ; i< 10;) {
+
+		int i = 0;
+
+		for (; i < 10;) {
 			System.out.println(++i);
 		}
-		
-		
 
 	}
 
