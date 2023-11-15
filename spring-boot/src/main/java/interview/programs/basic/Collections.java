77a158db9eb2daa9fabaec48f34a2f2074f7085f
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-11-15 12:47:23 +0530

    daily practice

--- spring-boot/src/main/java/interview/programs/basic/Collections.java
+++ spring-boot/src/main/java/interview/programs/basic/Collections.java
@@ -1,3 +1,4 @@
+package interview.programs.basic;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,26 +6,24 @@ public class Collections {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		List<String> list = new ArrayList<>();
 		list.add("a");
 		list.add("b");
-		list.add("c"); 
+		list.add("c");
 		list.add("d");
 		list.add("easdasdasd");
 		list.add("asd");
-		
+
 		printList(list);
-		
-		
 
 	}
 
 	private static void printList(List<String> list) {
-		for (int i = 0; i< list.size(); i++) {
+		for (int i = 0; i < list.size(); i++) {
 			System.out.println("element is -> " + list.get(i));
 		}
-		
+
 	}
 
 }
