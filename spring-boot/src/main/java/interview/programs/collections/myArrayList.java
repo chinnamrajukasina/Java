e0005b8d58ff0c756958b0ffcff240fe4dcc076b
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-11-15 12:47:08 +0530

    java initial commit2

--- spring-boot/src/main/java/interview/programs/collections/myArrayList.java
+++ spring-boot/src/main/java/interview/programs/collections/myArrayList.java
@@ -11,7 +11,6 @@ public class myArrayList {
 		myArrayList.add("Vijay");
 		myArrayList.add(0, "Raju");
 		myArrayList.add("Prasad");
-		//myArrayList.remove(myArrayList.size()-1);
 		System.out.println(myArrayList);
 		
 
