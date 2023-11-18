4e9734e5ed60ac16b706aeaeb19529b314cf454a
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-11-18 22:51:48 +0530

    word in Order

--- spring-boot/src/main/java/interview/programs/Infosys/WordsInOrder.java
+++ spring-boot/src/main/java/interview/programs/Infosys/WordsInOrder.java
@@ -1,16 +1,27 @@
 package interview.programs.Infosys;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 public class WordsInOrder {
 
 	public static void main(String[] args) {
-		String input ="  laptop mobile and desktop are not exempt  ";
-		String[]	words = input.split(" ")	;
-		System.out.println(words);
-		
+		String input = "laptop mobile and desktop are not exempt";
+		String output = sortWords(input);
+		System.out.println(output);
 
+	}
+
+	private static String sortWords(String input) {
+		// TODO Auto-generated method stub
+		String[] words = input.split(" ");
+		Arrays.sort(words,new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				int lengthComparisin = Integer.compare(s1.length(), s2.length());
+				return (lengthComparisin !=0)? lengthComparisin:s1.compareTo(s2);
+			}
+		});
+		
+		return String.join(" ", words);
 	}
 
 }
